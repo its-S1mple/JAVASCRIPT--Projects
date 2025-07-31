b69e4dc406406dbaeb3bdef1fe2fa53277c57b06
its-S1mple <manishprajapati5683@gmail.com>
2025-08-01 04:30:07 +0530

    made remove button in cart

--- shopping-cart APP/script.js
+++ shopping-cart APP/script.js
@@ -28,21 +28,35 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+
     function renderCart() {
         cartItemsEl.innerHTML = '';
         if (cart.length === 0) {
+            emptyCartEl.textContent = 'Your cart is empty!';
             emptyCartEl.style.display = 'block';
             cartTotalEl.classList.add('hidden');
+            cartItemsEl.style.display = 'none';
             return;
         }
         emptyCartEl.style.display = 'none';
         cartTotalEl.classList.remove('hidden');
+        cartItemsEl.style.display = 'flex';
 
         cart.forEach(item => {
             const cartItemEl = document.createElement('div');
             cartItemEl.className = 'cart-item';
             const quantityText = item.quantity > 1 ? ` x${item.quantity}` : '';
-            cartItemEl.textContent = `${item.name}${quantityText} - $${(item.price * item.quantity).toFixed(2)}`;
+            cartItemEl.innerHTML = `
+            <span class ="cart-item-name">${item.name}${quantityText} - $${(item.price * item.quantity).toFixed(2)}</span>
+            <button class ="remove-btn" data-id="${item.id}">Remove</button>
+            `;
+
+            cartItemEl.querySelector('.remove-btn')
+            .addEventListener('click', () => {
+                cart = cart.filter(cartItem => cartItem.id !== item.id);
+                renderCart();
+            });
+    
             cartItemsEl.appendChild(cartItemEl);
         });
 
@@ -69,6 +83,8 @@ document.addEventListener('DOMContentLoaded', () => {
             addToCart(id);
         }
     });
+
+    
 
     checkoutBtn.addEventListener('click', () => {
         if (cart.length === 0) {
